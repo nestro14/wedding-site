d362e88010f6c02b474af3b11b9bf6c1c00dc6be
Nestor Ceron <nestor_ceron@yahoo.com>
2017-11-05 17:53:57 -0800

    Adding rsvp functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,27 @@
 class PagesController < ApplicationController
   def index
   end
+
+  # def new
+  # end
+
+  # def show
+  #   @guest = Guest.find(params[:id])
+  # end
+
+  # def create
+  #   @rsvp_list = Guest.new(guest_params)
+
+  #   if(@post.save)
+  #     redirect_to @guest
+  #   else
+  #     render 'index'
+  #   end
+  # end
+
+  # private
+  #   def guest_params
+  #     params.require(:guest).permit(:full_name, :email)
+  #   end
+
 end
